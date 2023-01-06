20820173594149f51f669cd1e10580ace09fe742
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-01-06 10:11:17 +0000

    create environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    strapi_url: 'http://localhost:1337/api'
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    strapi_url: 'http://localhost:1337/api'
+  };
