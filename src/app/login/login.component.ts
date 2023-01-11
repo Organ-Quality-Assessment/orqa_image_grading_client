1ca88c36abb800565b8d48bfd7c69f6aab5d0da9
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-01-11 11:51:37 +0000

    user can log in and jwt saved to storage \6

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public loginForm: FormGroup | any;
+showAlert : boolean;
+message : String;
+
+constructor(
+  private authService: AuthService, private router: Router
+  )
+  {}
+
+  ngOnInit() {
+    this.showAlert = false;
+    this.message = '';
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  onSubmit() {
+  
+    this.removeAlert();
+      if (this.loginForm.get('email').valid && this.loginForm.get('password').valid) {
+        this.authService.login(
+        this.loginForm.get('email').value,
+        this.loginForm.get('password').value
+      )
+      .pipe()
+      .subscribe({
+        next: (resp) => { 
+          // handle jwt
+          this.authService.setSession(resp)
+          this.router.navigate([''])
+        },
+        error: (error) => {
+          console.log(error); 
+          this.showAlert = true; 
+          if (error['error']['error']['message']) {
+            this.message = error['error']['error']['message']
+          } else {
+            this.message = 'An error occured, please try again.'
+          }
+          
+        }
+      }
+      )
+    
+    
+      }
+      
+    }
+    removeAlert() {
+      this.message = '';
+      this.showAlert = false;
+    }
+    
+    goToPage(page: String) {
+      this.router.navigate([page])
+    }
 
 }
