8d7694eb8cc7c8912982e3bbabaf087e9bc36589
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-02-10 15:40:14 +0000

    question page done

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { any } from 'cypress/types/bluebird';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
+  private URL = environment.strapi_url
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  getSession() {
+    // todo if these items are not there or token is expired, redirect to login
+    return {
+      token: localStorage.getItem('orqa_token'),
+      id: localStorage.getItem('orqa_id')
+    }
+  }
+
+    submit(photography: any) {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + this.getSession().token,
+        'content-type': 'application/json'
+      })
+  
+      const data = {
+        data: {
+       isPhotographyValuable: photography
+        }     
+     }
+  
+      return this.http.post(this.URL + '/questions', data, {headers: headers})
+    }
+  }
+
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { QuestionsService } from '../questions.service';
 
 @Component({
   selector: 'app-questions',
@@ -7,12 +10,47 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent {
+  public form: FormGroup
 
+  constructor(private authService: AuthService, 
+    private questionService: QuestionsService,
+    private router: Router) {}
 
-  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      photography: new FormControl('', [Validators.required])      
+
+    })
+  }
 
   submit() {
-    // todo submit answers once finalised and log user out
+
+    // submit answers once finalised and log user out
+    if (this.form.get('photography').valid) {
+      if(this.form.get('photography').value === '1 - not important') {
+        this.form.get('photography').setValue('1')
+      }
+      else if(this.form.get('photography').value === '5 - very important') {
+        this.form.get('photography').setValue('5')
+      }
+this.questionService.submit(this.form.get('photography').value)
+.subscribe({
+  next: () => {
+    this.authService.logout();
+this.router.navigate(['register'])
+  },
+  error: (error) => {
+    console.log('error submitting form')
+  }
+})
+
+
+
+    } else {
+      console.log('problem with form')
+    }
+
+
   }
 
   logout() {
