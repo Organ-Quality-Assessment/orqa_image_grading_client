81e93366ec8c8d68337461fab01da79ec1075b9e
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-02-13 12:21:31 +0000

    update strapi prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    strapi_url: 'http://localhost:1337/api',
+    strapi_url: 'http://130.162.168.182:1337/api',
     strapi_image_url:'http://localhost:1337'
   };
