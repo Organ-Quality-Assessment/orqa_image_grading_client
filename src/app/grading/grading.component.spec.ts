591710e4b35862fec7352b17f07dc09f19a53253
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-03-03 13:30:05 +0000

    comparison tests

--- src/app/grading/grading.component.spec.ts
+++ src/app/grading/grading.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GradingComponent } from './grading.component';
 
@@ -8,7 +11,10 @@ describe('GradingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GradingComponent ]
+      declarations: [ GradingComponent ],
+      imports: [HttpClientModule, 
+      RouterTestingModule.withRoutes([])],
+      providers: []
     })
     .compileComponents();
 
