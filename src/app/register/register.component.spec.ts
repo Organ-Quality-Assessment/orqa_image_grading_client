8d9df3eff192ad09d4504e5ed585a855b9e7f053
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-03-09 14:15:29 +0000

    add tests to register component

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -42,10 +42,13 @@ const emailTakenError = {
 }
 
   beforeEach(async () => {
-    const authServiceSpy = jasmine.createSpyObj('authService', [
-      'register'
-    ])
-    authServiceSpy.register.and.returnValue(of(successResponse))
+    const authServiceSpy = jasmine.createSpyObj('AuthService', {
+      register:  { pipe: () => { return of(successResponse)} } ,
+      login:  { pipe: () => { return of(successResponse)} },
+      setSession:  { subscribe: () => { return successResponse} }
+    })
+    // authServiceSpy.register.and.returnValue(of(successResponse))
+    // authServiceSpy.login.and.returnValue(of(successResponse))
 
     await TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
@@ -79,7 +82,7 @@ const emailTakenError = {
     expect(field.valid).toBeFalsy(); 
   });
 
-  it('email field validity when not compelted correctly', () => {
+  it('email field validity when not compted correctly', () => {
     component.registerForm.patchValue({email: 'email.com'})
       let field = component.registerForm.controls['email']; 
       expect(field.valid).toBeFalsy(); 
@@ -120,21 +123,149 @@ it('username field validity when completed', () => {
         
       });
 
-it('should navigate to grading page when submit completed form', () => {
-  
-  
+it('should navigate to grading page when submit completed form with liver and kidney checked', () => { 
   component.registerForm.patchValue({password: 'password'})
     component.registerForm.patchValue({email: 'email@test.com'})
-    component.registerForm.patchValue({username: 'testing'})
-     
+    component.registerForm.patchValue({name: 'testing'})
+    component.registerForm.patchValue({institution: 'testing'})
+    component.registerForm.patchValue({job: 'testing'})
+    component.registerForm.patchValue({experience: 'testing'})
+    component.registerForm.patchValue({task: 'testing'})
+    component.registerForm.patchValue({liver: true})
+    component.registerForm.patchValue({kidney: true})
+   
     const routerSpy = spyOn(router, 'navigate')
+    
    
     fixture.detectChanges()
   component.onSubmit()
-  console.error(component.message)
   
-  expect(routerSpy).toHaveBeenCalledWith(['grading'])
+  expect(routerSpy).toHaveBeenCalledWith(['grading'], {queryParams: {organs: 'liver,kidney'}})
   })
+  
+
+
+  it('should navigate to grading page when submit completed form with liver checked and kidney not checked', () => { 
+    component.registerForm.patchValue({password: 'password'})
+      component.registerForm.patchValue({email: 'email@test.com'})
+      component.registerForm.patchValue({name: 'testing'})
+      component.registerForm.patchValue({institution: 'testing'})
+      component.registerForm.patchValue({job: 'testing'})
+      component.registerForm.patchValue({experience: 'testing'})
+      component.registerForm.patchValue({task: 'testing'})
+      component.registerForm.patchValue({liver: true})
+      component.registerForm.patchValue({kidney: false})
+     
+      const routerSpy = spyOn(router, 'navigate')
+      
+     
+      fixture.detectChanges()
+    component.onSubmit()
+    
+    expect(routerSpy).toHaveBeenCalledWith(['grading'], {queryParams: {organs: 'liver'}})
+    })
+  
+
+    it('should navigate to grading page when submit completed form with kidney checked and liver not checked', () => { 
+      component.registerForm.patchValue({password: 'password'})
+        component.registerForm.patchValue({email: 'email@test.com'})
+        component.registerForm.patchValue({name: 'testing'})
+        component.registerForm.patchValue({institution: 'testing'})
+        component.registerForm.patchValue({job: 'testing'})
+        component.registerForm.patchValue({experience: 'testing'})
+        component.registerForm.patchValue({task: 'testing'})
+        component.registerForm.patchValue({liver: false})
+        component.registerForm.patchValue({kidney: true})
+       
+        const routerSpy = spyOn(router, 'navigate')
+        
+       
+        fixture.detectChanges()
+      component.onSubmit()
+      
+      expect(routerSpy).toHaveBeenCalledWith(['grading'], {queryParams: {organs: 'kidney'}})
+      })
+
+      it('should navigate to comparison page when submit completed form with currently offered organs not checked', () => { 
+        component.registerForm.patchValue({password: 'password'})
+          component.registerForm.patchValue({email: 'email@test.com'})
+          component.registerForm.patchValue({name: 'testing'})
+          component.registerForm.patchValue({institution: 'testing'})
+          component.registerForm.patchValue({job: 'testing'})
+          component.registerForm.patchValue({experience: 'testing'})
+          component.registerForm.patchValue({task: 'testing'})
+          component.registerForm.patchValue({liver: false})
+          component.registerForm.patchValue({kidney: false})
+         
+          const routerSpy = spyOn(router, 'navigate')
+          
+         
+          fixture.detectChanges()
+        component.onSubmit()
+        
+        expect(routerSpy).toHaveBeenCalledWith(['comparison'])
+        })
+it('should create error message field if form fields are missing', () => {
+  expect(component.errorMessage).toEqual(undefined)
+  component.onSubmit()
+  expect(component.errorMessage).toEqual('Please complete all fields')
+})
+
+it('should return false if form fields are missing when check', () => {
+  expect(component.checkAllFieldsValid()).toBe(false)
+})
+
+it('should return true if form fields are present other than username', () => {
+  component.registerForm.patchValue({password: 'password'})
+  component.registerForm.patchValue({email: 'email@test.com'})
+  component.registerForm.patchValue({name: 'testing'})
+  component.registerForm.patchValue({institution: 'testing'})
+  component.registerForm.patchValue({job: 'testing'})
+  component.registerForm.patchValue({experience: 'testing'})
+  component.registerForm.patchValue({task: 'testing'})
+  component.registerForm.patchValue({liver: false})
+  component.registerForm.patchValue({kidney: false})
+
+  expect(component.checkAllFieldsValid()).toBe(true)
+})
+
+it('should correct convert experience to enum when less than a year', () => {
+  component.registerForm.patchValue({experience: 'less than 1 year'})
+
+  expect(component.convertExperienceToEnum(component.registerForm.get('experience').value)).toBe('less than one')
+})
+
+it('should correct convert experience to enum when 1-5 years', () => {
+  component.registerForm.patchValue({experience: '1-5 years'})
+
+  expect(component.convertExperienceToEnum(component.registerForm.get('experience').value)).toBe('one-five years')
+})
+
+it('should correct convert experience to enum when more than five', () => {
+  component.registerForm.patchValue({experience: 'five years or more'})
+
+  expect(component.convertExperienceToEnum(component.registerForm.get('experience').value)).toBe('five years or more')
+})
+
+it('should remove alert when submit form called', () => {
+  component.message = 'Error message'
+  component.showAlert = true
+  component.onSubmit()
+
+  expect(component.showAlert).toBeFalsy()
+  expect(component.message).toBe('')
+
+})
+
+it('should correctly build organ string for liver and kidney', () => {
+  component.registerForm.patchValue({liver: true})
+  component.registerForm.patchValue({kidney: true})
+
+  expect(component.buildStringFromCheckedOrgansCurrentlyGrading()).toBe('liver,kidney')
+})
+
+
+  // todo testing with getting errors back from auth service
 
   // it('should not navigate and show error when service returns error', () => {
   //   authServiceStub = {
